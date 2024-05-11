6a60583b536f878fe7fb1f01aff006c160e5a2f2
Sanket <sanketgupta1000@gmail.com>
2024-05-11 17:54:50 +0530

    Removing nested repo

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import Intro from "./Intro";
+
+function App() {
+    return (
+
+        <>
+            <h1>Hello World!</h1>
+            <Intro/>
+        </>
+    );
+}
+
+export default App;
---
+++ 01basicreact/src/Intro.js
@@ -0,0 +1,12 @@
+// when we create project with create-react-app, it doesn't have so many constraints
+// like components doen't need to have jsx extension
+// but their names need to start with uppercase
+
+function Intro()
+{
+    return (
+        <h2>Hello, my name is Sanket Gupta</h2>
+    )
+}
+
+export default Intro
